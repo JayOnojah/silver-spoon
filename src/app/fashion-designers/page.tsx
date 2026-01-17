34a2055d1c3c612b311a198eb6be63c2daaa6471
Elisha Momoh <ellydesignconcept@gmail.com>
2026-01-17 05:37:02 +0100

    completed 3 section in fashion design page

--- src/app/fashion-designers/page.tsx
+++ src/app/fashion-designers/page.tsx
@@ -1,12 +1,20 @@
 import { Navbar } from "@/components/_core/landing-pages/shared/navbar" 
 import { ForFashionDesignersHero } from "@/components/_core/landing-pages/for-fashion-designers/hero"   
-import Footer from "@/components/_core/landing-pages/shared/footer"
+import Footer from "@/components/_core/landing-pages/shared/footer" 
+import FashionBusiness from "@/components/_core/landing-pages/for-fashion-designers/fashion-business" 
+import { Operations } from "@/components/_core/landing-pages/for-fashion-designers/operations"
+import { ManageOrders } from "@/components/_core/landing-pages/for-fashion-designers/manage-orders"
+import { Marketing } from "@/src/components/_core/landing-pages/for-fashion-designers/marketing"
 
 export default function Page() {
     return (
         <div className="">
             <Navbar />
-            <ForFashionDesignersHero />
+            <ForFashionDesignersHero /> 
+            <FashionBusiness /> 
+            <Operations /> 
+            <ManageOrders />
+            <Marketing />
             <Footer />
         </div>
     )
