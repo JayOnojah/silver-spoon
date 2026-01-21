42b6ba2fe648680b00ad8bac8e41e116a25a9512
daviddozie <davidmgbede75@gmail.com>
2026-01-21 18:47:58 +0100

    feat: completed the academy page

--- src/app/academy/page.tsx
+++ src/app/academy/page.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next"
 import { Navbar } from "@/src/components/_core/landing-pages/shared/navbar"
+import { HeroAcademy } from "@/src/components/_core/landing-pages/academy/hero"
+import VideoTutorial from "@/src/components/_core/landing-pages/academy/video-tutorial"
+import FeaturedTutorials from "@/src/components/_core/landing-pages/academy/featured-tutorial"
+import Footer from "@/src/components/_core/landing-pages/shared/footer"
 
 export const metadata: Metadata = {
     title: "Silver Spoon Academy For Fashion Designers & Cobblers",
@@ -9,7 +13,11 @@ export const metadata: Metadata = {
 export default function Academy() {
     return (
         <>
-            <Navbar />
+            <Navbar bgNav="bg-white"/>
+            <HeroAcademy />
+            <VideoTutorial />
+            <FeaturedTutorials />
+            <Footer />
         </>
     )
 }
