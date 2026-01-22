51ab34edc6656adeac4cc32c01e1e2156d4d5b8e
daviddozie <davidmgbede75@gmail.com>
2026-01-22 17:18:42 +0100

    feat: added catalogue to dashboard

---
+++ src/app/dashboard/design-operations/catalogue/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { CatalogueDetails } from "@/src/components/_core/dashboard/design-operations/catalogue/catalogue-details";
+
+export const CatalogueDetailsPage = () => {
+    return <CatalogueDetails />;
+}
+
+export default CatalogueDetailsPage;
--- src/app/dashboard/design-operations/catalogue/page.tsx
+++ src/app/dashboard/design-operations/catalogue/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Button } from "@/src/components/ui/button";
+import { Plus } from "lucide-react";
+import { CatalogueItems } from "@/src/components/_core/dashboard/design-operations/catalogue/catalogue-items";
 
-const Catalogue = () => {
-  return <div>Catalogue</div>;
-};
+const Catelogue = () => {
+    return (
+        <>
+            <div className="font-sans flex flex-col md:flex-row justify-between gap-6 md:items-center">
+                <div>
+                    <h1 className="text-[#121926] text-2xl font-black mb-2">Catalogue</h1>
+                    <p className="text-[#9AA4B2]">Organize your designs into beautiful catalogues</p>
+                </div>
+                <Button className="bg-[#F74F25] text-white rounded-2xl h-12 font-bold">
+                    <Plus/>
+                    New Catalogue
+                </Button>
+            </div>
+            <CatalogueItems />
+        </>
+    )
+}
 
-export default Catalogue;
+export default Catelogue;
