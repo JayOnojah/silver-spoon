c9e4057e61c03c0ea36368cb038ae47a1dc8b93f
Elisha Momoh <ellydesignconcept@gmail.com>
2026-01-26 19:06:44 +0100

    moodboard

--- src/app/dashboard/design-operations/mood-boards/page.tsx
+++ src/app/dashboard/design-operations/mood-boards/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { MoodboardItems } from "@/src/components/_core/dashboard/design-operations/catalogue/moodboard-items";
+import { AddMoodboard } from "@/src/components/_core/dashboard/design-operations/catalogue/add-moodboard";
 
-const MoodBoard = () => {
-  return <div>MoodBoard</div>;
-};
+const Moodboard = () => {
+    return (
+        <>
+            <div className="font-sans flex flex-col md:flex-row justify-between gap-6 md:items-center">
+                <div>
+                    <h1 className="text-[#121926] text-2xl font-black mb-2">Moodboards</h1>
+                    <p className="text-[#9AA4B2]">Organize your inspiration boards for your projects and customers</p>
+                </div>
+               <AddMoodboard btnName="New Moodboard"/>
+            </div>
+            <MoodboardItems />
+        </>
+    )
+}
 
-export default MoodBoard;
+export default Moodboard;
---
+++ src/components/_core/dashboard/design-operations/catalogue/add-moodboard.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState } from 'react';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from '@/components/ui/dialog';
+import { Button } from '@/src/components/ui/button';
+
+
+import { Plus } from 'lucide-react';
+
+interface AddMoodboardModalProps {
+    onSubmit?: (data: { name: string; description: string }) => void;
+    btnName: string;
+}
+
+export const AddMoodboard = ({btnName }: 
+    AddMoodboardModalProps) => {
+    const [open, setOpen] = useState(false);
+   
+    
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button className="bg-[#F74F25] text-white rounded-2xl h-12 font-bold font-sans">
+                    <Plus />
+                    {btnName}
+                </Button>
+            </DialogTrigger>
+
+            <DialogContent className="sm:max-w-140 p-0 gap-0 rounded-4xl border font-sans">
+                <div className="relative">
+                    <DialogHeader className="md:px-10 px-4 md:pt-8 pt-4 text-start">
+                        <DialogTitle className="text-2xl font-black text-[#121926]">
+                            Add Moodboard
+                        </DialogTitle>
+                        <p className="text-sm text-[#9AA4B2]">
+                            source
+                        </p>
+                    </DialogHeader>
+
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+};
