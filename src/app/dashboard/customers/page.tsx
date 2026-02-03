ba4258484054b0b0f264eb13be22b543b928d3b9
daviddozie <davidmgbede75@gmail.com>
2026-02-03 10:14:16 +0100

    chore: added stuff

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
-const Customers = () => {
-  return (
-    <div>
-      customers
-    </div>
-  )
+const Customer = () => {
+    return (
+        <>
+            <div className="font-sans">
+                <h1 className="text-2xl font-black text-[#121926]">Customer</h1>
+            </div>
+        </>
+    )
 }
 
-export default Customers
+export default Customer;
---
+++ src/components/_core/dashboard/customer/index.tsx
@@ -0,0 +1,12 @@
+import { UsersRound } from "lucide-react"
+
+export const AllCustomer = () => {
+    return (
+        <div className="font-sans">
+            <div className="bg-white p-6 rounded-2xl">
+                <UsersRound className="text-[#F74F25]" />
+                <h1 className="text-[#121926]">300</h1>
+            </div>
+        </div>
+    )
+}
