0a1cb980f3d4f5fe2829627edd9e28623bf76ac4
delaney <delaneysylvans@gmail.com>
2026-02-19 00:55:17 +0100

    SS-29: Delete stale migrations

--- drizzle/0006_overconfident_riptide.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "phone" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "password" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "google_id" text;--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_google_id_unique" UNIQUE("google_id");
